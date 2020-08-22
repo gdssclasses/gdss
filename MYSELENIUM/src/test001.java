36bb11629616ddb458c934abd26a38072a25e19c
HP <HP@DESKTOP-DAN7M6B>
2020-08-22 14:00:27 +0530

    this is java comit

---
+++ MYSELENIUM/src/test001.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;  
+import org.openqa.selenium.JavascriptExecutor;  
+import org.openqa.selenium.WebDriver;  
+import org.openqa.selenium.chrome.ChromeDriver;  
+
+public class test001 {
+
+   public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	   System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Desktop\\Driver\\chromedriver.exe"); 
+	   WebDriver driver=new ChromeDriver();  
+	   driver.navigate().to("http://www.javatpoint.com/");  
+   
+   }
+
+}
