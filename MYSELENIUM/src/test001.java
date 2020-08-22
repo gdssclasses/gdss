c832e6b7168f01a013e4020fadc2ae8ccfc788d0
HP <HP@DESKTOP-DAN7M6B>
2020-08-22 14:02:38 +0530

    this is my java

--- MYSELENIUM/src/test001.java
+++ MYSELENIUM/src/test001.java
@@ -2,7 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;  
 import org.openqa.selenium.WebDriver;  
 import org.openqa.selenium.chrome.ChromeDriver;  
-
+/* this is  */
 public class test001 {
 
    public static void main(String[] args) {
