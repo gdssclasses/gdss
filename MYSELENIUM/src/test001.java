6f9cc73ef948e6efe3b3b5d78bedb7e1ba9fbe6b
HP <HP@DESKTOP-DAN7M6B>
2020-08-24 21:27:58 +0530

    this is my new commit

--- MYSELENIUM/src/test001.java
+++ MYSELENIUM/src/test001.java
@@ -2,7 +2,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;  
 import org.openqa.selenium.WebDriver;  
 import org.openqa.selenium.chrome.ChromeDriver;  
-/* this is  */
+/* this is 
+ * my name 
+ *  jjjjj
+ *  */
 public class test001 {
 
    public static void main(String[] args) {
